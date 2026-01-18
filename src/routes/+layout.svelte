3a27545369e1e4738f64d94512cc535f6427ee9a
Andy Li <andyli@kth.se>
2026-01-18 22:33:39 +0100

    fix: use vercel analytics injectors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import { Analytics } from '@vercel/analytics/sveltekit';
-	import { SpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectAnalytics();
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
 
 <!-- blank root layout -->
 {@render children()}
-<Analytics />
-<SpeedInsights />
