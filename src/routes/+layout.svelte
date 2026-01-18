3260e07cfe3c358f93057b32cf4eb90a1d9ce3e6
Andy Li <andyli@kth.se>
2026-01-18 22:29:23 +0100

    feat: add vercel analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import { Analytics } from '@vercel/analytics/sveltekit';
+	import { SpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	let { children } = $props();
 </script>
 
 <!-- blank root layout -->
 {@render children()}
+<Analytics />
+<SpeedInsights />
